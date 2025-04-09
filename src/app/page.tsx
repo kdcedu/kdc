be47ade097418d261c61ba292cbf7d128d9a4922
nguyenlong <ad.nguyenlong@gmail.com>
2025-04-09 13:16:47 +0700

    first-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,71 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import OTPInput from "react-otp-input";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [otp, setOtp] = useState("");
+  const [isError, setIsError] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const trueOtp = "1234";
+
+  const handleChange = (value: string) => {
+    if(value.length === 4) {
+      setIsError(value !== trueOtp)
+    }
+    else setIsError(false);
+
+    setOtp(value);
+  }
+
+  return (
+    <div className="w-screen h-screen">
+      <div className="w-full h-fit bg-[#f5712e]">
+        <Image
+          src="/headers/header.png"
+          alt="Login Header"
+          width={200}
+          height={100}
+          className="w-full h-auto"
+          sizes="100vw"
+        />
+      </div>
+
+      <div className="flex w-full">
+        <Image
+          src="/backgrounds/loginBackground.png"
+          alt="Login Header"
+          width={500}
+          height={500}
+          quality={100}
+          className="w-[45%]"
+        />
+
+        <div className="border-4 rounded-xl border-orange-400 m-auto p-5">
+          <h1 className="text-4xl font-semibold text-[#f5712e] text-center">Đăng nhập</h1>
+          <div className="text-xl text-center text-orange-400 my-5">Bạn hãy vui lòng nhập mã PIN để tham gia hoạt động</div>
+          <div className="flex justify-center">
+              <OTPInput
+                value={otp}
+                onChange={handleChange}
+                numInputs={4}
+                renderInput={(props) => <input {...props} />}
+                inputStyle={{
+                  width: "2.5rem",
+                  height: "2.5rem",
+                  margin: "0 0.5rem",
+                  fontSize: "1.5rem",
+                  borderRadius: "8px",
+                  border: `3px solid ${isError ? "red" : "#f5712e"}`,
+                  textAlign: "center",
+                  color: "#333",
+                  transition: "border-color 0.3s",
+                }}
+              />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
