c320c9a559a5fa2c5e1b8dd0e4698c6aa38c231d
nguyenlong <ad.nguyenlong@gmail.com>
2025-05-06 11:29:31 +0700

    add background game

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,9 +1,11 @@
 import { Image } from "antd";
 
 export default function GamePage() {
-  return <div className="w-screen h-screen bg-[url(/backgrounds/gameBackground.jpg)] bg-cover">
-    <div className="absolute mt-[250px] ml-[200px]">
-      <Image alt="Test" preview={false} width={200} src="/images/grade.jpg"/>
+  return <div className="bg-black flex items-center justify-center min-w-screen min-h-screen">
+    <div className="w-[1000px] h-[625px] bg-[url(/backgrounds/gameBackground.jpg)] bg-cover">
+      <div className="absolute mt-[180px] ml-[150px]">
+        <Image alt="Test" preview={false} width={100} src="/images/grade.jpg"/>
+      </div>
     </div>
-  </div>
+  </div> 
 }
