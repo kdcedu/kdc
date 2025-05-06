600bc86e9e11e1e430200ae12d2f4593810f1057
nguyenlong <ad.nguyenlong@gmail.com>
2025-05-06 11:57:43 +0700

    add padding

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,7 +1,7 @@
 import { Image } from "antd";
 
 export default function GamePage() {
-  return <div className="w-screen h-screen bg-black overflow-x-auto">
+  return <div className="w-screen h-screen bg-black overflow-x-auto mt-12">
   <div className="w-[1250px] h-[780px] bg-[url('/backgrounds/gameBackground.jpg')] bg-cover relative m-auto">
     <div className="absolute top-[250px] left-[200px]">
       <Image alt="Test" width={100} preview={false} src="/images/grade.jpg" />
@@ -9,3 +9,9 @@ export default function GamePage() {
   </div>
 </div>
 }
+
+// export default function GamePage() {
+//   return <div className="w-screen h-screen bg-[url('/backgrounds/gameBackground.jpg')] bg-cover">
+//     Hello
+//   </div>
+// }
