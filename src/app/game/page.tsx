54e9cc5735f72764888ea36ef27dc32e7d6bc8c3
nguyenlong <ad.nguyenlong@gmail.com>
2025-05-06 11:36:16 +0700

    change size

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,7 +2,7 @@ import { Image } from "antd";
 
 export default function GamePage() {
   return <div className="bg-black flex items-center justify-center min-w-screen min-h-screen">
-    <div className="w-[1000px] h-[625px] bg-[url(/backgrounds/gameBackground.jpg)] bg-cover">
+    <div className="w-[1250px] h-[780px] bg-[url(/backgrounds/gameBackground.jpg)] bg-cover">
       <div className="absolute mt-[180px] ml-[150px]">
         <Image alt="Test" preview={false} width={100} src="/images/grade.jpg"/>
       </div>
